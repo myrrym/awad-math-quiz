a671c78ed5226490c128cd4b2f0a9c861c83c916
myrrym <miriamho1998@gmail.com>
2023-03-09 08:50:43 +0000

    added container-fluid

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -20,6 +20,20 @@
         </div>
 
     </div>
+
+    <div class="block-answer">
+
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+
+    </div>
 @endsection
 
 @section("script")
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -15,27 +15,29 @@
     @yield("head")
 </head>
 <body>
-    
-    @if ($navbar == 'without-options')
-        <x-navbar-without-options/>
-    @else
-        <x-navbar-with-options/>
-    @endif
-    
-    <div class="row">
-        <div class="col-12">
-            @yield("content")
-            <!-- cdn - bootstrap js -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <div class="container-fluid">
+        
+        @if ($navbar == 'without-options')
+            <x-navbar-without-options/>
+        @else
+            <x-navbar-with-options/>
+        @endif
+        
+        <div class="row">
+            <div class="col-12">
+                @yield("content")
+                <!-- cdn - bootstrap js -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+            </div>
         </div>
-    </div>
-    
-    @if ($footer == 'true')
-        <x-footer/>
-    @else
-        <!-- nothing -->
-    @endif
+        
+        @if ($footer == 'true')
+            <x-footer/>
+        @else
+            <!-- nothing -->
+        @endif
 
+    </div>
     @yield("script")
 </body>
 </html>
