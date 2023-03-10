adf7d0f0ef37010b0e562ad1a5cbd54c9c20e1bc
myrrym <miriamho1998@gmail.com>
2023-03-10 01:13:53 +0000

    added favicon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math ᓚᘏᗢ Cat</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/logo-math-cat.png">
 
     <!-- cdn - jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" type="text/javascript"></script>
