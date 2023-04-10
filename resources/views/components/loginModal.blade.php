97aea2c8224f786cf63354746650215197a7e350
unknown <ayj123@1utar.my>
2023-04-10 17:37:11 +0800

    implementing login api

--- resources/views/components/loginModal.blade.php
+++ resources/views/components/loginModal.blade.php
@@ -11,17 +11,16 @@
                     {{-- form for the login --}}
                     <form action="/api/login" method="POST">
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" name="username" id="username">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password" id="password">
                         </div>
                         <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                            <input type="checkbox" class="form-check-input" name="remember_me" id="check">
+                            <label class="form-check-label" for="check">Remember Me</label>
                         </div>
                         <div>
                             <!-- Button trigger modal -->
