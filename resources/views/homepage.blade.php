6f7bab94b84a09faa307fecf23b53724d1829dcf
DonutEmperor <56782553+DonutEmperor@users.noreply.github.com>
2023-04-13 16:11:35 +0800

    Javascript(catpaw-animation) comments removed

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -38,31 +38,6 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script>
         $(document).ready(function(){
-            // $('*').hover(function(){
-            //     if($(this).is('.easy')){
-                    
-            //         $('.cat-paw-1').animate({
-            //             opacity:"1",
-            //         },500);
-            //     }else if($(this).is('.medium')){
-
-            //         $('.cat-paw-2').animate({
-            //             opacity:"1",
-            //         },500);
-            //     }else if($(this).is('.hard')){
-
-            //         $('.cat-paw-3').animate({
-            //             opacity:"1",
-            //         },500);
-            //     }else if($(this).is('.whatTheMeow')){
-
-            //         $('.cat-paw-4').animate({
-            //             opacity:"1",
-            //         },500);
-            //     }
-            // })
-
-            // redirect player based on difficulty
             $(".js-card").click(function(){
                 var val = $(this).data('diff');
                 
@@ -126,85 +101,4 @@
             });
         });
     </script>
-    <!-- <script>
-        $(document).ready(function(){
-            $(".difficulty").mouseover(function(){
-            if ($(this).hasClass("easy")) {
-                $(".cat-paw-1").animate({
-                opacity:"1",
-                },500);
-            } else if ($(this).hasClass("medium")) {
-                $(".cat-paw-2").animate({
-                opacity:"1",
-                },500);
-            } else if ($(this).hasClass("hard")) {
-                $(".cat-paw-3").animate({
-                opacity:"1",
-                },500);
-            } else if ($(this).hasClass("whatTheMeow")) {
-                $(".cat-paw-4").animate({
-                opacity:"1",
-                },500);
-            }
-            });
-        });
-</script> -->
-<!-- <script>
-        $(document).ready(function(){
-            $(".easy").mouseenter(function(){
-                // $('.cat-paw-2').css('opacity','0');
-                // $('.cat-paw-3').css('opacity','0');
-                // $('.cat-paw-4').css('opacity','0');
-                $('.cat-paw-1').animate({
-                        opacity:"1",
-                    },200);
-            });
-            $(".medium").mouseenter(function(){
-                // $('.cat-paw-1').css('opacity','0');
-                // $('.cat-paw-3').css('opacity','0');
-                // $('.cat-paw-4').css('opacity','0');
-                $('.cat-paw-2').animate({
-                        opacity:"1",
-                    },200);
-            });
-            $(".hard").mouseenter(function(){
-                // $('.cat-paw-2').css('opacity','0');
-                // $('.cat-paw-1').css('opacity','0');
-                // $('.cat-paw-4').css('opacity','0');
-                $('.cat-paw-3').animate({
-                        opacity:"1",
-                    },200);
-            });
-            $(".whatTheMeow").mouseenter(function(){
-                // $('.cat-paw-2').css('opacity','0');
-                // $('.cat-paw-3').css('opacity','0');
-                // $('.cat-paw-1').css('opacity','0');
-                $('.cat-paw-4').animate({
-                        opacity:"1",
-                    },200);
-            });
-            
-            $(".easy").mouseout(function(){
-                $('.cat-paw-2').css('opacity','0');
-                $('.cat-paw-3').css('opacity','0');
-                $('.cat-paw-4').css('opacity','0');
-            });
-            $(".medium").mouseout(function(){
-                $('.cat-paw-1').css('opacity','0');
-                $('.cat-paw-3').css('opacity','0');
-                $('.cat-paw-4').css('opacity','0');
-            });
-            $(".hard").mouseout(function(){
-                $('.cat-paw-2').css('opacity','0');
-                $('.cat-paw-1').css('opacity','0');
-                $('.cat-paw-4').css('opacity','0');
-            });
-            $(".whatTheMeow").mouseout(function(){
-                $('.cat-paw-2').css('opacity','0');
-                $('.cat-paw-3').css('opacity','0');
-                $('.cat-paw-1').css('opacity','0');
-            });
-            
-        });
-</script> -->
 @endsection
