80c43461ec50059b189adcf61273b3d6fa99bace
unknown <ayj123@1utar.my>
2023-04-14 15:26:31 +0800

    change some prompt

--- resources/views/components/authmessage.blade.php
+++ resources/views/components/authmessage.blade.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header border-0" <h5 class="modal-title" id="alertModalLabel">Login Message</h5>
+            <div class="modal-header border-0" <h5 class="modal-title" id="alertModalLabel">Messages</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
